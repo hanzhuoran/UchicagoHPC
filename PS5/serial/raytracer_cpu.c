060326240e4a2616d2cf6ac9548cd6ff7fea0ff8
Zhuoran Han <zhuoranh@mit.edu>
2018-07-18 16:44:07 -0500

    fix bug

--- PS5/serial/raytracer_cpu.c
+++ PS5/serial/raytracer_cpu.c
@@ -81,6 +81,7 @@ void rayTrace(int px, int py, int nrays, Camera camera) {
 		int i, j;
 		double delta, t, brightness;
 		double dotp_vc;
+		double scaler;
 		vec3d vec_v; // view ray vector
 		vec3d vec_i; // position of intersection
 		vec3d vec_s; // direction of light source at I
@@ -89,20 +90,35 @@ void rayTrace(int px, int py, int nrays, Camera camera) {
 
 		do { // sample random V from unit sphere
 			vec_v = randomDirection();
-			vec_w = vecmul(vec_v, camera.pos.y / vec_v.y);
+
+			scaler = camera.pos.y/vec_v.y;
+			// printf("scaler is %lf\n", scaler);
+			vec_w = vecmul(vec_v, scaler);
+
+			// printf("vx %lf \n", vec_v.x);
+			// printf("vy %lf \n", vec_v.y);
+			// printf("vz %lf \n", vec_v.z);
+
+
+			// printf("cx %lf \n", vec_c.x);
+			// printf("cy %lf \n", vec_c.y);
+			// printf("cz %lf \n", vec_c.z);
+			
 			dotp_vc = vecdot(vec_v, vec_c);
+			// printf("v dot c = %lf \n", dotp_vc);
 			delta = dotp_vc*dotp_vc + radius*radius - dotp_cc;
 		} while (vec_w.x < w_min_x || vec_w.x > w_max_x || vec_w.z < w_min_z || vec_w.z > w_max_z || delta < 0);
 
 		t = dotp_vc - sqrt(delta);
-
 		vec_i = vecmul(vec_v, t);
 		vec_n = vecsub(vec_i, vec_c);
 		vec_n = vecmul(vec_n, 1.0 / norm(vec_n));
 		vec_s = vecsub(vec_l, vec_i);
 		vec_s = vecmul(vec_s, 1.0 / norm(vec_s));
+		// printf("%lf \n", vec_s.x);
 
 		brightness = vecdot(vec_s, vec_n);
+		// printf("brightness: %lf \n",brightness);
 		brightness = MAX(brightness,0);
 
 		j = px - 1 - (int) ((double) px * (vec_w.x - w_min_x) / (camera.width));
